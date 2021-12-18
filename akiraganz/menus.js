c1931fba2d49a2df04dcd3f6634c96cc3dc867bd
Clicknetcafe <rama4shared@gmail.com>
2021-12-18 16:49:05 +0800

    Add files via upload

---
+++ akiraganz/menus.js
@@ -0,0 +1,181 @@
+const menus = (prefix, namabot) => {
+    return `├ • ${prefix}menugenshin
+├ • ${prefix}menuanime
+├ • ${prefix}menunsfw
+├ • ${prefix}menuoxy
+├ • ${prefix}menuephoto
+├ • ${prefix}menutextpro
+├ • ${prefix}menueditor
+│
+├──「 *BOT MENU* 」
+├ • ${prefix}verify
+├ • ${prefix}info
+├ • ${prefix}ping
+├ • ${prefix}rules
+├ • ${prefix}owner
+├ • ${prefix}setnamabot
+├ • ${prefix}antivirtex
+│
+├──「 *MOVIE & STORY* 」
+├ • ${prefix}lk21 query
+├ • ${prefix}drakorongoing
+├ • ${prefix}wattpad url_wattpad
+├ • ${prefix}wattpadsearch query
+├ • ${prefix}cerpen
+├ • ${prefix}ceritahoror
+│
+├──「 *SEARCHING* 」
+├ • ${prefix}shopee query
+├ • ${prefix}google query
+├ • ${prefix}gimage query
+├ • ${prefix}gimage2 query
+├ • ${prefix}konachan query
+├ • ${prefix}playstore query
+├ • ${prefix}wallpapersearch query
+├ • ${prefix}wallpapersearch2 query
+│
+├──「 *RANDOM TEXT* 」
+├ • ${prefix}quotes
+├ • ${prefix}quotesdilan
+├ • ${prefix}quotesanime
+├ • ${prefix}quotesimage
+├ • ${prefix}faktaunik
+├ • ${prefix}katabijak
+├ • ${prefix}pantun
+├ • ${prefix}bucin
+├ • ${prefix}randomnama
+│
+├──「 *INFORMATION* 」
+├ • ${prefix}jadwaltv channel
+├ • ${prefix}jadwaltvnow
+├ • ${prefix}jadwalbola
+├ • ${prefix}qrreader
+├ • ${prefix}heroml hero_name
+├ • ${prefix}mlstalk id/server
+├ • ${prefix}superhero character
+├ • ${prefix}wikipedia query
+├ • ${prefix}translate kode_negara|text
+├ • ${prefix}brainly query
+├ • ${prefix}newsinfo
+├ • ${prefix}cnnindonesia
+├ • ${prefix}cnnnasional
+├ • ${prefix}cnninternasional
+├ • ${prefix}infogempa
+├ • ${prefix}lirik query
+├ • ${prefix}chord query
+├ • ${prefix}cuaca daerah
+├ • ${prefix}kodepos query
+├ • ${prefix}indbeasiswa
+├ • ${prefix}hoax
+├ • ${prefix}nsfwcheck
+├ • ${prefix}ocr
+│
+├──「 *ENTERTAINMENT* 」
+├ • ${prefix}bts
+├ • ${prefix}exo
+├ • ${prefix}blackpink
+├ • ${prefix}cecan
+├ • ${prefix}cogan
+├ • ${prefix}asupan
+├ • ${prefix}wancak
+├ • ${prefix}meme
+├ • ${prefix}meme2
+├ • ${prefix}darkjoke
+│
+├──「 *CREATOR* 」
+├ • ${prefix}ttp text
+├ • ${prefix}ttp2 text
+├ • ${prefix}ttp3 text
+├ • ${prefix}ttp4 text
+├ • ${prefix}attp text
+├ • ${prefix}sticker
+├ • ${prefix}stickerwa
+├ • ${prefix}toimg
+├ • ${prefix}smoji emoji
+├ • ${prefix}fakedonald text
+│
+├──「 *PRIMBON* 」
+├ • ${prefix}artinama nama
+├ • ${prefix}jodoh nama1|nama2 
+├ • ${prefix}weton tgl|bln|tahun
+├ • ${prefix}jadian tgl|bln|tahun
+├ • ${prefix}tebakumur name
+│
+├──「 *FUN MENU* 」
+├ • ${prefix}bisakah
+├ • ${prefix}kapankah
+├ • ${prefix}apakah
+├ • ${prefix}tebakgambar
+├ • ${prefix}tebakbendera
+├ • ${prefix}caklontong
+├ • ${prefix}tebakjenaka
+├ • ${prefix}tebakchara
+├ • ${prefix}siapaaku
+├ • ${prefix}asahotak
+├ • ${prefix}susunkata
+├ • ${prefix}batal _(batalkan kuis)_
+│
+├──「 *OTHER MENU* 」
+├ • ${prefix}wame
+├ • ${prefix}readmore
+├ • ${prefix}spamcall 823xxxxxxxx
+├ • ${prefix}osuplayer
+│
+├──「 *LIMIT MENU* 」
+├ • ${prefix}limit
+├ • ${prefix}buylimit
+├ • ${prefix}dompet
+├ • ${prefix}transfer @tag|nominal
+│
+├──「 *GROUP MENU* 」
+├ • ${prefix}mining
+├ • ${prefix}hidetag
+├ • ${prefix}del
+├ • ${prefix}level
+├ • ${prefix}leaderboard
+├ • ${prefix}linkgc
+├ • ${prefix}tagall
+├ • ${prefix}setpp
+├ • ${prefix}add
+├ • ${prefix}kick
+├ • ${prefix}setname
+├ • ${prefix}setdesc
+├ • ${prefix}demote
+├ • ${prefix}promote
+├ • ${prefix}listadmin
+├ • ${prefix}group [buka/tutup]
+├ • ${prefix}leveling [enable/disable]
+├ • ${prefix}nsfw [1/0]
+├ • ${prefix}simih [1/0]
+├ • ${prefix}antilink [1/0]
+├ • ${prefix}welcome [1/0]
+│
+├──「 *OWNER MENU* 」
+├ • ${prefix}bc
+├ • ${prefix}bcgc
+├ • ${prefix}kickall
+├ • ${prefix}setreply
+├ • ${prefix}addlimit
+├ • ${prefix}setlimit
+├ • ${prefix}setnamabot
+├ • ${prefix}antilinkgrup [1/0]
+├ • ${prefix}antidelete [1/0]
+├ • ${prefix}clearall
+├ • ${prefix}ban
+├ • ${prefix}unban
+├ • ${prefix}block
+├ • ${prefix}unblock
+├ • ${prefix}setmemberlimit
+├ • ${prefix}addbadword <teks>
+├ • ${prefix}delbadword <teks>
+├ • ${prefix}listbadword
+├ • ${prefix}nobadword [enable/disable]
+├ • ${prefix}listblock
+├ • ${prefix}leave
+├ • ${prefix}event [1/0]
+├ • ${prefix}clone
+├ • ${prefix}setbotpp
+│
+╰───「 ${namabot} 」`
+}
+exports.menus = menus
